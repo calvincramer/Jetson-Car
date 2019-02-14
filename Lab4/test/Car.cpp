28c43d8dcb84f7475a8511d5d4930f618d6d5c83
Calvin Cramer <cjcramer@ucdavis.edu>
2019-02-13 16:33:40 -0800

    Got better controls

--- Lab4/test/Car.cpp
+++ Lab4/test/Car.cpp
@@ -24,11 +24,11 @@
 #define STEERING_NEUTRAL 284
 #define SHORT_MAX 32767
 #define SHORT_MIN -32768
-#define DEAD_ZONE 1000
+#define DEAD_ZONE 1000	// Applied in the range [SHORT_MIN, SHORT_MAX]
 
 int Car::to_range(int val, int val_min, int val_max, int new_min, int new_max)
 {
-//math might need work
+	// Tested and works
 	double n = (val - val_min) * 1.0 / (val_max - val_min);
 	n *= (new_max - new_min);
 	n += new_min;
@@ -45,41 +45,43 @@ Car::Car(PCA9685 *driver, SDL_Joystick *joy)
 int Car::readJoystick()
 {
 // fixed code to look more like repo
-	SDL_Delay(10);
-	bool something_new = false;
+	SDL_Delay(10);	// In ms
+	bool new_steering_event = false;
+	bool new_motor_event = false;
 	int steering = 0;
 	int motor = 0;
 	int quit = 0;
 	SDL_Event event;
 	while (SDL_PollEvent(&event)){
-		something_new = true;
-
-		int old_value = 0;
   		switch(event.type) {
 			case SDL_JOYAXISMOTION:
-				if (event.jaxis.axis == 0) {
-					//if ((old_value + 10000 > event.jaxis.value) || (old_value - 10000< event.jaxis.value))
+				if (event.jaxis.axis == 0) {	// STEERING EVENT
+					new_steering_event = true;
 					steering = event.jaxis.value;
-					//old_value = event.jaxis.value;
 				}
-				else if(event.jaxis.axis == 5) {
-					motor = event.jaxis.value;
+				if (event.jaxis.axis == 5) {	// MOTOR EVENT
+					new_motor_event = true;
+					motor = -1 * event.jaxis.value;
 				}
 				break;
 			case SDL_QUIT:
 				std::cout << "quitting\n";
 				quit = 0;
 				break;
-			default:
-				//std::cout << "I dont care about this event hahahahaha\n";
+			default:	 // Do nothing about other events
 				break;
 		}
 	}
-	if (something_new){
+	if (new_steering_event) {
 		setSteeringAngle(steering);
-		setCarThrottle(motor); 
+		new_steering_event = false;
 	}
-	//std::cout << "HELLO: " << quit << std::endl;
+	if (new_motor_event) {
+		setCarThrottle(motor);
+		new_motor_event = false;
+	}
+
+
 	return quit;
 }
 
@@ -89,8 +91,8 @@ void Car::setSteeringAngle(int value)
 		return;
 
 	// Dead zone
-	//if (abs(value) < DEAD_ZONE)
-	//	value = 0;
+	if (abs(value) < DEAD_ZONE)
+		value = 0;
 
 	int pca_value = to_range(value, SHORT_MIN, SHORT_MAX, STEERING_MAX_LEFT, STEERING_MAX_RIGHT);
 	
@@ -110,8 +112,8 @@ void Car::setCarThrottle(int value)
 		return;
 	
 	// Dead zone
-	//if (abs(value) < DEAD_ZONE)
-	//	value = 0;
+	if (abs(value) < DEAD_ZONE)
+		value = 0;
 
 	int pca_value = to_range(value, SHORT_MIN, SHORT_MAX, THROTTLE_MAX_REVERSE, THROTTLE_MAX_FORWARD);
 	
@@ -127,7 +129,6 @@ void Car::setCarThrottle(int value)
 
 int main(int argc, char** argv) {
 	// Initialize SDL, this makes the joystick available
-
 	SDL_SetHint(SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, "1");
 
 	if(SDL_Init(SDL_INIT_JOYSTICK) < 0) {
@@ -159,25 +160,24 @@ int main(int argc, char** argv) {
 		std::cout << "no PCA9685\n";
 		return 1;
 	}
-	std::cout << "Setting PCA frequency\n" << std::flush;
+	
  	printf("PCA9685 Device Address: 0x%02X\n",pca->kI2CAddress) ;
 	std::cout << "BUS NUMBER: " << (int) pca->kI2CBus << std::endl;
-	pca->setAllPWM(0, 0);
-	pca->reset();
-	pca->setPWMFrequency(50);
-	std::cout << "after setPWMfrequency" << std::endl;
-	pca->setPWM(ESC_CHANNEL, 0, THROTTLE_NEUTRAL);
-	pca->setPWM(STEERING_CHANNEL, 0, STEERING_NEUTRAL);
+	
 	std::cout << "Setting PCA PWM to 0\n" << std::flush;
-	//pca->setAllPWM(0, 0);
+	pca->setAllPWM(0, 0);
 
-	std::cout << "Setting PCA STEERING\n" << std::flush;
-	//pca->setPWM(STEERING_CHANNEL, 0, STEERING_NEUTRAL);
+	pca->reset();
+	
+	std::cout << "Setting PCA frequency\n" << std::flush;
+	pca->setPWMFrequency(50);
+	//std::cout << "after setPWMfrequency" << std::endl;
 
 	std::cout << "Setting PCA THROTTLE\n" << std::flush;
-	//pca->setPWM(ESC_CHANNEL, 0, THROTTLE_NEUTRAL);
+	pca->setPWM(ESC_CHANNEL, 0, THROTTLE_NEUTRAL);
 
-	//return 1;
+	std::cout << "Setting PCA STEERING\n" << std::flush;
+	pca->setPWM(STEERING_CHANNEL, 0, STEERING_NEUTRAL);
 
 	// Initialize car object
 	Car* car = new Car(pca, joy);
@@ -188,10 +188,9 @@ int main(int argc, char** argv) {
 	//int error = SDL_PollEvent(&event);
 	//std::cout << "error: " << error << std::endl;
 	int quit = 0;
-//NEEDED TWO WHILE LOOPS, INNER ONE JUST FOR POOLING
-	while(!car->readJoystick()) {
 
-  	}
+	//NEEDED TWO WHILE LOOPS, INNER ONE JUST FOR POOLING
+	while(!car->readJoystick());
 
 	return 0;
 }
